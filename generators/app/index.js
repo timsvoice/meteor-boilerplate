21bbb6230c9536d272a1646e9444bebda6a45db1
Timothy Voice <timsethvoice@gmail.com>
2016-04-09 10:54:39 -0400

    eslinting in progress

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,20 +4,20 @@
 
 
 'use strict';
-let yeoman = require('yeoman-generator');
-let chalk = require('chalk');
-let yosay = require('yosay');
-let path = require('path');
+
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+var path = require('path');
 
 module.exports = yeoman.Base.extend({
 
-  constructor: function () {
-    yeoman.generators.Base.apply(this, arguments);
+  constructor() {
+    yeoman.Base.apply(this, arguments);
     // appname flag
-    this.argument('appname', {type: String, required: false});
-    //set appname from argument-defined appname or get the path
+    this.argument('appname', { type: String, required: false });
+    // set appname from argument-defined appname or get the path
     this.appname = this.appname || path.basename(process.cwd());
-    // create packages array for adding to .meteor/packages later
+    // standard packages for any meteor app
     this.packages = [
       'meteor-base',
       'mobile-experience',
@@ -30,62 +30,63 @@ module.exports = yeoman.Base.extend({
       'es5-shim',
       'ecmascript',
       'accounts-password',
-      'useraccounts:core'
+      'useraccounts:core',
     ];
   },
 
-  prompting: function () {
-    let done = this.async();
-
+  prompting() {
+    const done = this.async();
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the polished ' + chalk.red('Meteor 1.3 Boilerplate') + ' generator!'
-    ));    
+    this.log(
+      yosay(
+        'Welcome to the polished Meteor 1.3 Boilerplate generator!'
+        )
+      );
 
-    let prompts = [{
+    const prompts = [{
       type: 'input',
       name: 'name',
       message: 'Name: What would you like to call your app?',
-      default: this.appname
-    },{
+      default: this.appname,
+    }, {
       type: 'list',
       name: 'rendering',
       message: 'Render Engine: Which rendering engine would you like to use?',
       choices: [
         'Blaze',
         'React',
-        'Other'
+        'Other',
       ],
-      default: 'Blaze'
-    },{
+      default: 'Blaze',
+    }, {
       type: 'confirm',
       name: 'lint',
       message: 'ESLint: Would you like to configure this project with the AirBnB ESLint module?',
-      default: true
-    },{
+      default: true,
+    }, {
       type: 'list',
       name: 'frontend',
       message: 'Frontend: Do you use any of these frontend frameworks??',
       choices: [
         'Foundation',
-        'Bootstrap',
-        'Nope'
+        'Nope',
       ],
-      default: 'Foundation'
+      default: 'Foundation',
     }];
 
-    this.prompt(prompts, function (answers) {
+    this.prompt(prompts, answers => {
       this.answers = answers;
       // To access answers later use this.answers.someAnswer;
       done();
-    }.bind(this));
+      return this.answers;
+    });
   },
 
-  scaffolding: function () {
+  scaffolding() {
     this.mkdir('client');
     this.mkdir('public');
     this.mkdir('server');
-    this.mkdir('.meteor')
+    this.mkdir('.meteor');
 
     this.mkdir('imports');
     this.mkdir('imports/api');
@@ -93,69 +94,108 @@ module.exports = yeoman.Base.extend({
 
     this.mkdir('imports/startup');
     this.mkdir('imports/startup/client');
-    
+
     this.mkdir('imports/startup/server');
     this.mkdir('imports/startup/server/development');
     this.mkdir('imports/startup/server/production');
   },
 
-  writing: function () {
-    let context = {
-      appname: this.answers.name
-    };
+  writing() {
+    // context for template processing
+    // const context = {
+    //   appname: this.answers.name,
+    // };
 
-    this.copy('.meteor/release', '.meteor/release');
-    this.copy('package.json', 'package.json');
-    this.copy('client/main.js', 'client/main.js');
-    this.copy('server/main.js', 'server/main.js');
-    this.copy('imports/startup/client/index.js', 'imports/startup/client/index.js');
-    this.copy('imports/startup/client/routes.js', 'imports/startup/client/routes.js');
-    this.copy('imports/startup/client/accounts-config.js', 'imports/startup/client/accounts-config.js');
-    this.copy('imports/startup/server/index.js','imports/startup/server/index.js');
-    this.copy('imports/startup/server/fixtures.js','imports/startup/server/fixtures.js');
-    this.copy('imports/startup/server/development/settings.json','imports/startup/server/development/settings.json');
-    this.copy('imports/startup/server/production/settings.json','imports/startup/server/production/settings.json');
+    this.copy(
+      '.meteor/release',
+      '.meteor/release'
+    );
+    this.copy(
+      'package.json',
+      'package.json'
+    );
+    this.copy(
+      'client/main.js',
+      'client/main.js'
+    );
+    this.copy(
+      'server/main.js',
+      'server/main.js'
+    );
+    this.copy(
+      'imports/startup/client/index.js',
+      'imports/startup/client/index.js'
+    );
+    this.copy(
+      'imports/startup/client/routes.js',
+      'imports/startup/client/routes.js'
+    );
+    this.copy(
+      'imports/startup/client/accounts-config.js',
+      'imports/startup/client/accounts-config.js'
+    );
+    this.copy(
+      'imports/startup/server/index.js',
+      'imports/startup/server/index.js'
+    );
+    this.copy(
+      'imports/startup/server/fixtures.js',
+      'imports/startup/server/fixtures.js'
+    );
+    this.copy(
+      'imports/startup/server/development/settings.json',
+      'imports/startup/server/development/settings.json'
+    );
+    this.copy(
+      'imports/startup/server/production/settings.json',
+      'imports/startup/server/production/settings.json'
+    );
   },
 
-  meteorPackages: function () {
+  meteorPackages() {
     // install a rendering engine
-    switch (this.answers.rendering) { 
+    switch (this.answers.rendering) {
       case 'Blaze':
-        this.packages.push('kadira:blaze-layout') 
+        this.packages.push('kadira:blaze-layout');
         break;
       case 'React':
-        this.packages.push('kadira:react-layout')
+        this.packages.push('kadira:react-layout');
         break;
       case 'Other':
-        this.log('No Rendering Engine included')
+        this.log('No Rendering Engine included');
+        break;
+      default:
+        this.log('No Rendering Engine included');
         break;
     }
-
-    this.packages.push('kadira:flow-router','aldeed:collection2');    
+    // add flowrouter and collection2
+    this.packages.push('kadira:flow-router', 'aldeed:collection2');
+    // add selected packages to Meteor
     this.write('.meteor/packages', this.packages.join('\n'));
   },
 
-  installNpm: function () {
-    if (this.answers.lint) {
-      this.npmInstall(['eslint', 'eslint-config-airbnb'], {'saveDev': true});
-    }
-    
-    if (this.answers.rendering === 'React') {
-      this.npmInstall(['eslint-plugin-react'], {'saveDev': true});
-    }
-    
+  installNpm() {
+    // Install a front end framework
     switch (this.answers.frontend) {
       case 'Foundation':
-        this.npmInstall('foundation-sites', {'save': true})
-        break;
-      case 'Bootstrap':
-        this.npmInstall('bootstrap', {'save': true})
+        this.npmInstall('foundation-sites', { save: true });
         break;
       case 'Nope':
-        thos.log('No frontend framework installed')
+        this.log('No frontend framework installed');
+        break;
+      default:
+        this.log('No frontend framework installed');
     }
-
-    this.installDependencies();
-  },  
+    // install linter if selected
+    if (this.answers.lint) {
+      this.npmInstall(['eslint', 'eslint-config-airbnb'], { saveDev: true });
+    }
+    // install react linter is selected
+    if (this.answers.rendering === 'React') {
+      this.npmInstall(['eslint-plugin-react'], { saveDev: true });
+    }
+    // install npm dependencies
+    this.npmInstall();
+  },
 
 });
